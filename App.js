f545e97c28b5f17483efb423427fa44e2aded1b8
tarun2506 <tarunsharma8920@gmail.com>
2021-08-21 18:17:38 +0530

    initialized the project and did setup

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import Home from "./screens/home";
+import Home from "./components/home";
 
 export default function App() {
   return <Home />;
